1761f760452da57210018e93d1e4fef39968ed84
AirCraft009 <coconraupe@gmail.com>
2025-12-08 23:32:18 +0100

    day 5 initial commit

---
+++ Day5/target/debug/deps/Day5-30cdfda212c7e2db.d
@@ -0,0 +1,5 @@
+C:\Users\cocon\Documents\programming\Portfolio\AdventOfCode2025\Day5\target\debug\deps\Day5-30cdfda212c7e2db.d: src\main.rs
+
+C:\Users\cocon\Documents\programming\Portfolio\AdventOfCode2025\Day5\target\debug\deps\libDay5-30cdfda212c7e2db.rmeta: src\main.rs
+
+src\main.rs:
